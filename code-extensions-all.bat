5eb184ffa3a530b92eb1479d4cc347c4a62564c0
codyconfer <mail@codyconfer.me>
2019-03-20 14:57:52 -0600

    add install all batch

---
+++ code-extensions-all.bat
@@ -0,0 +1,5 @@
+if exist code-extensions-ide.bat code-extensions-ide.bat
+if exist code-extensions-cs.bat code-extensions-cs.bat
+if exist code-extensions-js.bat code-extensions-js.bat
+if exist code-extensions-vue.bat code-extensions-vue.bat
+if exist code-extensions-themes.bat code-extensions-themes.bat
