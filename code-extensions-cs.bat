5363d3d4a145dad799f78ffb5a7affa6ad3dc732
codyconfer <Cody.Confer@flyfrontier.com>
2019-03-27 09:07:46 -0600

    add more csharp extensions

--- code-extensions-cs.bat
+++ code-extensions-cs.bat
@@ -1,5 +1,5 @@
 @echo off
 
 echo [CS] - Installing csharp extensions...
-code --install-extension jchannon.csharpextensions --install-extension ms-vscode.csharp
+code --install-extension jchannon.csharpextensions --install-extension ms-vscode.csharp --install-extension k--kato.docomment --install-extension austincummings.razor-plus --install-extension formulahendry.dotnet --install-extension formulahendry.dotnet-test-explorer --install-extension formulahendry.azure-storage-explorer
 goto :eof
