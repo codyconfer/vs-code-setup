94a7becd8307aae91c92985ff3f0ef3394040e69
Cody Confer <mail@codyconfer.me>
2019-05-07 18:09:02 -0700

    adding insiders scripts

---
+++ code-insiders-extensions-themes.bat
@@ -0,0 +1,5 @@
+@echo off
+
+echo [THEMES] - Installing theme and icon pack extensions...
+code-insiders --install-extension erik-rosengren.dark-candy --install-extension ms-vscode.Theme-MaterialKit --install-extension PKief.material-icon-theme --install-extension wesbos.theme-cobalt2 --install-extension ms-vscode.Theme-MarkdownKit --install-extension SolarLiner.linux-themes --install-extension teabyii.ayu --install-extension vscode-icons-team.vscode-icons --install-extension file-icons.file-icons --install-extension formulahendry.theme-ubuntu-style
+goto :eof
