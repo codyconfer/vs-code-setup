5260c3b55026c3001aed2c267044078a71bbe315
codyconfer <mail@codyconfer.me>
2019-07-13 11:49:07 -0600

    remove old batch scripts

--- code-extensions-js.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-echo [JS] - Installing JavaScript extensions...
-code --install-extension dbaeumer.vscode-eslint --install-extension christian-kohler.npm-intellisense --install-extension joelday.docthis --install-extension leizongmin.node-module-intellisense --install-extension nickdodd79.gulptasks --install-extension eg2.vscode-npm-script --install-extension ms-vscode.vscode-typescript-tslint-plugin
-goto :eof
--- code-insiders-extensions-vue.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-echo [VUE] - Installing vue.js extensions...
-code-insiders --install-extension dariofuzinato.vue-peek --install-extension mubaidr.vuejs-extension-pack --install-extension octref.vetur
-goto :eof
